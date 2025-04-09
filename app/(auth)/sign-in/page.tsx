dc59ecf495d79c10eb3cbf867ad8af71a2375e29
Vishnu <vishnusharma2k03@gmail.com>
2025-04-09 22:50:13 +0530

    refactor: implement authentication form with validation and UI components

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,12 @@
+import Authform from '@/components/Authform'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <Authform type = {"sign-in"}/>
+    </section>
   )
 }
 
-export default SignIn
+export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
+import Authform from '@/components/Authform'
 import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <Authform type = {"sign-up"}/>
+    </section>
   )
 }
 
-export default SignUp
+export default SignUp;
